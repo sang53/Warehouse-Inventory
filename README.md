af25d1280306120e626126335e3ce0d2d0c6f6f1
sang53 <143582855+sang53@users.noreply.github.com>
2025-04-11 00:51:32 +1000

    add config files, README

---
+++ README.md
@@ -0,0 +1,3 @@
+## The Odin Project Backend Repo
+
+This contains the files and projects created for The Odin Project Backend course using Node.js
---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  eslintConfigPrettier,
+);
