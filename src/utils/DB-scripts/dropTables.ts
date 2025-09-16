2675a7f40287811f220a7c2dc9684a72fd0ee2ef
sang53 <143582855+sang53@users.noreply.github.com>
2025-09-16 18:46:18 +1000

    Fixed DB scripts
    Changed dropTypes & dropTables to allow dropping
    when some defined types & tables were not in the database
    Previously, it would rollback on any error

--- src/utils/DB-scripts/dropTables.ts
+++ src/utils/DB-scripts/dropTables.ts
@@ -2,19 +2,11 @@ import db from "../../config/pool.ts";
 import { TNAMES } from "../../config/tableSchema.ts";
 
 async function dropTables() {
-  const client = await db.connect();
-  try {
-    await client.query("BEGIN");
-    await client.query(
-      `DROP TABLE ${Object.values(TNAMES).join(", ")} CASCADE;`,
-    );
-    await client.query("COMMIT");
-  } catch (error) {
-    console.error("Error dropping tables: ", error);
-    await client.query("ROLLBACK");
-  } finally {
-    client.release();
-  }
+  await Promise.allSettled(
+    Object.values(TNAMES).map((table) =>
+      db.query(`DROP TABLE ${table} CASCADE;`),
+    ),
+  );
 }
 
 await dropTables();
--- src/utils/DB-scripts/dropTypes.ts
+++ src/utils/DB-scripts/dropTypes.ts
@@ -2,19 +2,11 @@ import db from "../../config/pool.ts";
 import tableSchema from "../../config/tableSchema.ts";
 
 async function dropTypes() {
-  const client = await db.connect();
-  try {
-    await client.query("BEGIN");
-    await client.query(
-      `DROP TYPE ${Object.keys(tableSchema.TYPES).join(", ")} CASCADE;`,
-    );
-    await client.query("COMMIT");
-  } catch (error) {
-    console.error("Error dropping tables: ", error);
-    await client.query("ROLLBACK");
-  } finally {
-    client.release();
-  }
+  await Promise.allSettled(
+    Object.keys(tableSchema.TYPES).map((type) =>
+      db.query(`DROP TYPE ${type} CASCADE;`),
+    ),
+  );
 }
 
 await dropTypes();
