08b6e2bc88ea826fe0a34d8c871f0314ba75053d
sang53 <143582855+sang53@users.noreply.github.com>
2025-11-07 17:31:13 +1100

    Update db pool configs to be configurable

--- src/config/pool.ts
+++ src/config/pool.ts
@@ -4,8 +4,8 @@ import "dotenv/config";
 types.setTypeParser(23, (val) => Number(val));
 
 export default new Pool({
-  host: "localhost",
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  database: "top_users",
+  database: process.env.DB_NAME,
   password: process.env.DATABASE_PASSWORD,
 });
