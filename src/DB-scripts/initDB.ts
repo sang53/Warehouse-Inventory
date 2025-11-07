25f8b71a4ffd5222049511edac17d4daca85eb4f
sang53 <143582855+sang53@users.noreply.github.com>
2025-11-07 20:31:30 +1100

    changed initDB script to reset DB

--- src/DB-scripts/initDB.ts
+++
@@ -1,7 +0,0 @@
-import { initData } from "./initData.js";
-import { createTables } from "./utils/createTables.js";
-import { createTypes } from "./utils/createTypes.js";
-
-await createTypes();
-await createTables();
-await initData();
